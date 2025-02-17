6f76c29b98f1a134737694980aecbccaf7b46afb
Jerry <juber.kudchi@gmail.com>
2025-02-17 14:37:23 +0530

    Added new project

---
+++ 04_ice-cream_picker.py
@@ -0,0 +1,10 @@
+import random
+
+# List of names
+names = ["Mango", "Vanilla", "Chocolate", "Black Current", "Paan", "Afghan Dry-Fruit", "American Nuts", "Shaalimar", "Epic", "Sundae", "Brownie", "Kaju Drajsha", "Punjabi kulfi", "Rajvadi kulfi", "Raspberry dolly", "Faluda", "Chocolate cone", "Dark Chocolate cone", "Belgian Chocolate cone", "Butterscotch", "Butterscotch cone", "Black Current cone", "Gud Bud", "Maza", "Badam Kulfi", "Pista kulfi", "Rajbhog","Frostic", "Chocobar", "Nothing"]
+
+# Select a random name from the list
+random_name = random.choice(names)
+
+# Print the selected name
+print(f"Let's Eat: {random_name} Today")
